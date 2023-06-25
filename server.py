5fcb4f1d06aabc9dccb3fd843ce27440231a9dca
henriquePinheiro <48884506+HenriqueMartinelliPinheiro@users.noreply.github.com>
2023-06-25 10:28:18 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,51 +1,51 @@
-# server.py
-
 import socket
 import threading
 import os
 
-def handle_client(client_socket, client_address):
-    print(f'[+] Handling connection from {client_address}')
+def receberArquivo(socketCliente, ipCliente):
+    print(f'Processando requisição de: {ipCliente}')
 
-    # Recebendo o nome do arquivo
-    file_name = client_socket.recv(1024).decode()
+    nomeArquivo = socketCliente.recv(1024).decode() #recebe o nome do arquivo com no máximo 1024 bytes
 
-    # Recebendo o tamanho do arquivo
-    file_size = int(client_socket.recv(1024).decode())
-    print(f'[+] Receiving file {file_name} of size {file_size} bytes')
+    tamanhoArquivo = int(socketCliente.recv(1024).decode()) # recebe o tamnho do arquivo e converte em int
+    print(f'Recebendo Arquivo{nomeArquivo}')
 
-    # Recebendo o arquivo
-    with open(file_name, 'wb') as f:
-        bytes_received = 0
-        while bytes_received < file_size:
-            chunk = client_socket.recv(1024)
-            f.write(chunk)
-            bytes_received += len(chunk)
-            progress = (bytes_received / file_size) * 100
-            print(f'[+] {file_name} - {progress:.2f}%')
+    diretorio = "arquivos" # diretório onde o arquivo será salvo
+    os.makedirs(diretorio, exist_ok=True) # se o diretório não existir é criado
+    caminhoArquivo = os.path.join(diretorio, nomeArquivo) # caminho completo do arquivo
 
-    print(f'[+] File {file_name} received successfully')
+    with open(caminhoArquivo, 'wb') as arquivo: 
+        bytesEnviados = 0 # quantidade de bytes recebidos até o momento
+        while bytesEnviados < tamanhoArquivo:
+            dados = socketCliente.recv(1024) # recebe 1024 bytes por vez
+            arquivo.write(dados) # escreve no arquivo
+            bytesEnviados += len(dados)
+            progresso = (bytesEnviados / tamanhoArquivo) * 100 # progresso do upload
+            print(f'{nomeArquivo} - {progresso:.2f}%')
 
-    client_socket.close()
+    print(f'Arquivo {nomeArquivo} recebido')
+
+    socketCliente.close() # encerra a conexão
 
 
 def main():
-    server_ip = '0.0.0.0'
-    server_port = 12345
+    ipServer = '0.0.0.0'
+    portaServer = 12345
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((server_ip, server_port))
-    server.listen(5)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # cria o socket
+    server.bind((ipServer,portaServer)) # vincula o socket ao IP e porta
+    server.listen(5) # fica esperando conexões, no máximo 5
 
-    print(f'[+] Server listening on {server_ip}:{server_port}')
+    print(f'[+] Servidor Aguradando em {ipServer}:{portaServer}')
 
     try:
         while True:
-            client_socket, client_address = server.accept()
-            client_handler = threading.Thread(target=handle_client, args=(client_socket, client_address))
-            client_handler.start()
+            socketCliente, ipCliente = server.accept() # aceita conexão do socket do cliente
+            requisicaoCliente = threading.Thread(target=receberArquivo, args=(socketCliente, ipCliente))
+            # cria a thread para a função receberArquivo
+            requisicaoCliente.start()
     except KeyboardInterrupt:
-        print('\n[+] Server shutting down')
+        print('\n[+] Desligando Servidor')
         server.close()
 
 
